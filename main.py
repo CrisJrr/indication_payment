50aa5f2b62ccef0a9f7366e93b137f581f85c82a
Cris Brandt <cristiano.brandt@finanto.com.br>
2025-04-01 17:49:11 -0300

    aaaa

--- main.py
+++ main.py
@@ -4,9 +4,10 @@ import pandas as pd
 from api_functions import puxa, pay, read
 
 base = pd.read_excel(r"pagamentos.xlsx")
+base_csv = pd.read_csv(r"pagamentos.csv")
 
-cpf = base["CPF"]
-valor = base["Valor"]
+cpf = base_csv["cpf"]
+valor = base_csv["valor"]
 
 for c, v in zip(cpf, valor):
     try:
