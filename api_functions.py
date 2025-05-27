b9412becce912b612fe6472186ee5eea5dba3580
Cris Brandt <crisbrandtjr@gmail.com>
2025-05-27 17:36:01 -0300

    atualizações de logs e registro de base

--- api_functions.py
+++ api_functions.py
@@ -1,10 +1,13 @@
 import pandas as pd
 import requests
+import os
 from requests import Session
+from datetime import datetime
 
 url = "https://api.finantopay.com.br"
 headers = {'accept': '*/*'}
-# print(cli)
+parquet_path = "indication_payment/indication_register.parquet"
+
 
 def puxa(cpf, session):
         find_url = f'{url}/CustomerIndication/Find?documentNumber=/{cpf}'
@@ -22,20 +25,38 @@ def puxa(cpf, session):
             print(f"Erro de conexão: {e}")
 
 
-def pay(cpf, session):
+def pay(cpf, session, valor):
     cpf = cpf.rjust(11, '0')
     pay_url = f'{url}/CustomerIndication/CommissionDisbursedToClient?documentNumber={cpf}'
     response = session.put(pay_url, timeout= 120)
     if response.status_code == 200:
         retorno = response.json()
-        print(f"CPF: {cpf} feito\n")
+        print(f"CPF {cpf} pago\n")
+        register(cpf, valor, 'Pago')
 
 
 def read(cpf, valor):
     with Session() as session:
         doc, payment_amount, data = puxa(cpf, session)
-        print(f"\nCPF: {doc}\nPayment Amount: {payment_amount}")
         if valor == payment_amount:
-            pay(doc, session)
+            pay(doc, session, valor)
         else:
-            print(f"Valor incorreto: \nOriginal: {payment_amount}\nRegistrado: {valor}\n")
+            print(f"Verificar valor do CPF: {cpf}\n")
+            register(doc,valor, 'Não pago, valor divergente')
+
+def register(cpf, valor, status):
+    new_data = pd.DataFrame([{
+        'cpf': cpf,
+        'valor': valor,
+        'event_datetime': datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+        'status': status
+    }])
+
+    if os.path.exists(parquet_path):
+        df_existing = pd.read_parquet(parquet_path)
+        df = pd.concat([df_existing, new_data], ignore_index=True)
+    else:
+        df = new_data
+    
+    df.to_parquet(parquet_path, index=False, engine='pyarrow')
+
--- main.py
+++ main.py
@@ -2,17 +2,30 @@ import requests
 from requests import Session
 import pandas as pd
 from api_functions import puxa, pay, read
+import os
 
-# base = pd.read_excel(r"pagamentos.xlsx")
-base_csv = pd.read_csv(r"pagamentos.csv")
 
-cpf = base_csv["cpf"]
-valor = base_csv["valor"]
+def listar_arquivos_xlsx(pasta_raiz):
+    arquivos_xlsx = None
+    
+    for raiz, _, arquivos in os.walk(pasta_raiz):
+        for arquivo in arquivos:
+            if arquivo.lower().endswith('.xlsx'):
+                caminho_completo = os.path.join(raiz, arquivo)
+                arquivos_xlsx = caminho_completo
+    
+    return arquivos_xlsx
+
+path = listar_arquivos_xlsx("/home/crisbrandt/Documentos/github/cris/indication/indication_payment/")
+
+base = pd.read_excel(path, header=3)
+
+cpf = base["CPF/CNPJ"]
+valor = base["VALOR"]
 
 for c, v in zip(cpf, valor):
     try:
         c = str(c).rjust(11, '0')
         doc, payment = read(c, v)
-        print(doc, payment)
     except:
         pass
