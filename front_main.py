849548225793a7bb17c97f85d24e887da615642e
CrisJrr <crisbrandtjr@gmail.com>
2025-06-11 09:43:13 -0300

    creditos

--- front_main.py
+++ front_main.py
@@ -11,3 +11,12 @@ from functions.objects import *
 
 upload_file()
 indication_confirm()
+
+st.markdown(
+    """
+    <div style="text-align: center; font-size: 12px; color: gray; margin-top: 50px;">
+        Feito por Cris
+    </div>
+    """,
+    unsafe_allow_html=True
+)
